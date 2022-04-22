170687071ee4f7a6b3854bf0aaf8a0ed02d9dd30
Andrew Peck <andrew.peck@cern.ch>
2022-04-22 11:00:48 -0400

    dataformat: apply black for consistent python formatting

--- src/etroc/configs/dataformat_parser.py
+++ src/etroc/configs/dataformat_parser.py
@@ -1,8 +1,9 @@
 import yaml
 
 filename = "dataformat"
-stream = open(filename+".yaml", "r")
+stream = open(filename + ".yaml", "r")
 ROC2dat = yaml.safe_load(stream)["ETROC2"]
+
 
 def toVHDhex(num, length=0):
     return 'x"{num:0{width}X}"'.format(num=num, width=length)
@@ -12,7 +13,8 @@ def mask2range(num):
     binnum = bin(num)[2:]
     start = len(binnum) - 1
     end = len(binnum) - binnum.rfind("1") - 1
-    return str(start)+" downto "+str(end)
+    return str(start) + " downto " + str(end)
+
 
 def defConstants(dat):
     data = dat["identifiers"]
@@ -21,27 +23,37 @@ def defConstants(dat):
     for const in data:
         for typ in data[const]:
             length = len(str.upper(const) + str.upper(typ))
-            if (length > max):
+            if length > max:
                 max = length
 
     for const in data:
         for typ in data[const]:
-            vhd.write("  constant %s_IDENTIFIER_%s%s: std_logic_vector (39 downto 0) := %s;\n" % (
+            vhd.write(
+                "  constant %s_IDENTIFIER_%s%s: std_logic_vector (39 downto 0) := %s;\n"
+                % (
                     str.upper(const),
                     str.upper(typ),
-                    " "*(max-len(str.upper(const) + str.upper(typ))),
-                    toVHDhex(data[const][typ], 10)))
+                    " " * (max - len(str.upper(const) + str.upper(typ))),
+                    toVHDhex(data[const][typ], 10),
+                )
+            )
+
 
 def defSubTypes(dat, name):
     data = dat["data"][name]
-    vhd.write("  -- "+name+"\n")
+    vhd.write("  -- " + name + "\n")
     for const in reversed(data):
-        vhd.write("  subtype "+str.upper(const)+"_RANGE is natural range "
-            +mask2range(data[const]["mask"])+";\n")
+        vhd.write(
+            "  subtype "
+            + str.upper(const)
+            + "_RANGE is natural range "
+            + mask2range(data[const]["mask"])
+            + ";\n"
+        )
     vhd.write("\n")
 
 
-vhd = open(filename+".vhd", "w")
+vhd = open(filename + ".vhd", "w")
 
 vhd.write("library ieee;\n")
 vhd.write("use ieee.std_logic_1164.all;\n")
@@ -49,13 +61,15 @@ vhd.write("use ieee.std_logic_misc.all;\n")
 vhd.write("use ieee.numeric_std.all;\n")
 vhd.write("\n")
 
-vhd.write("package "+filename+"_pkg is"+"\n")
+vhd.write("package " + filename + "_pkg is" + "\n")
 
 vhd.write("\n")
 
 vhd.write("  constant REVERSE : boolean := ")
-if ROC2dat["bitorder"] == "reversed": vhd.write("true")
-else: vhd.write("false")
+if ROC2dat["bitorder"] == "reversed":
+    vhd.write("true")
+else:
+    vhd.write("false")
 vhd.write(";\n\n")
 
 defConstants(ROC2dat)
@@ -66,4 +80,4 @@ defSubTypes(ROC2dat, "header")
 defSubTypes(ROC2dat, "data")
 defSubTypes(ROC2dat, "trailer")
 
-vhd.write("end package "+filename+"_pkg;")
+vhd.write("end package " + filename + "_pkg;")
--- src/etroc/configs/fast_commands_parser.py
+++ src/etroc/configs/fast_commands_parser.py
@@ -1,27 +1,31 @@
 import yaml
 
 filename = "fast_commands"
-stream = open(filename+".yaml", "r")
+stream = open(filename + ".yaml", "r")
 ROC2dat = yaml.safe_load(stream)["ETROC2"]
+
 
 def toVHDhex(num, length=0):
     VHDhex = str(hex(num))
-    if length == 0: length = len(VHDhex)
-    return VHDhex[1]+"\""+str.upper(VHDhex[2:])[:length]+"\""
+    if length == 0:
+        length = len(VHDhex)
+    return VHDhex[1] + '"' + str.upper(VHDhex[2:])[:length] + '"'
+
 
 def writeData(dat, vhd):
     for const in dat:
-        vhd.write("\tconstant "+const+"_CMD\t: ")
-        vhd.write("std_logic_vector(7 downto 0) := "+toVHDhex(dat[const])+";\n")
+        vhd.write("\tconstant " + const + "_CMD\t: ")
+        vhd.write("std_logic_vector(7 downto 0) := " + toVHDhex(dat[const]) + ";\n")
     return
 
-vhd = open(filename+".vhd", "w")
+
+vhd = open(filename + ".vhd", "w")
 
 vhd.write("library ieee;\n")
 vhd.write("use ieee.std_logic_1164.all;\n")
 vhd.write("use ieee.std_logic_misc.all;\n")
 vhd.write("use ieee.numeric_std.all;\n")
 vhd.write("\n")
-vhd.write("package "+filename+"_pkg is"+"\n")
+vhd.write("package " + filename + "_pkg is" + "\n")
 writeData(ROC2dat, vhd)
-vhd.write("end package "+filename+"_pkg;")
+vhd.write("end package " + filename + "_pkg;")
