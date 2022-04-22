92bfa0a9f800331030240639ef80b0681ba63520
Andrew Peck <andrew.peck@cern.ch>
2022-04-22 09:40:22 -0400

    dataformat: fix vhdl syntax and align the : char in constants

--- src/etroc/configs/dataformat.vhd
+++ src/etroc/configs/dataformat.vhd
@@ -3,40 +3,40 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_misc.all;
 use ieee.numeric_std.all;
 
-constant REVERSE : boolean := true;
-
-constant HEADER_IDENTIFIER_FRAME	: std_logic_vector (39 downto 0) := x"3C5C000000";
-constant HEADER_IDENTIFIER_MASK	: std_logic_vector (39 downto 0) := x"FFFFC00000";
-constant DATA_IDENTIFIER_FRAME	: std_logic_vector (39 downto 0) := x"8000000000";
-constant DATA_IDENTIFIER_MASK	: std_logic_vector (39 downto 0) := x"8000000000";
-constant FILLER_IDENTIFIER_FRAME	: std_logic_vector (39 downto 0) := x"3C5C800000";
-constant FILLER_IDENTIFIER_MASK	: std_logic_vector (39 downto 0) := x"FFFFC00000";
-constant TRAILER_IDENTIFIER_FRAME	: std_logic_vector (39 downto 0) := x"0000000000";
-constant TRAILER_IDENTIFIER_MASK	: std_logic_vector (39 downto 0) := x"8000000000";
-
 package dataformat_pkg is
 
+  constant REVERSE : boolean := true;
+
+  constant HEADER_IDENTIFIER_FRAME : std_logic_vector (39 downto 0) := x"3C5C000000";
+  constant HEADER_IDENTIFIER_MASK  : std_logic_vector (39 downto 0) := x"FFFFC00000";
+  constant DATA_IDENTIFIER_FRAME   : std_logic_vector (39 downto 0) := x"8000000000";
+  constant DATA_IDENTIFIER_MASK    : std_logic_vector (39 downto 0) := x"8000000000";
+  constant FILLER_IDENTIFIER_FRAME : std_logic_vector (39 downto 0) := x"3C5C800000";
+  constant FILLER_IDENTIFIER_MASK  : std_logic_vector (39 downto 0) := x"FFFFC00000";
+  constant TRAILER_IDENTIFIER_FRAME: std_logic_vector (39 downto 0) := x"0000000000";
+  constant TRAILER_IDENTIFIER_MASK : std_logic_vector (39 downto 0) := x"8000000000";
+
   -- header
-  subtype BCID_RANGE is natural range 11 down to 0;
-  subtype TYPE_RANGE is natural range 13 down to 12;
-  subtype L1COUNTER_RANGE is natural range 21 down to 14;
+  subtype BCID_RANGE is natural range 11 downto 0;
+  subtype TYPE_RANGE is natural range 13 downto 12;
+  subtype L1COUNTER_RANGE is natural range 21 downto 14;
 
   -- data
-  subtype TOT_RANGE is natural range 8 down to 0;
-  subtype CAL_RANGE is natural range 18 down to 9;
-  subtype TOA_RANGE is natural range 29 down to 19;
-  subtype COUNTER_A_RANGE is natural range 8 down to 0;
-  subtype BCID_RANGE is natural range 20 down to 9;
-  subtype ROW_ID2_RANGE is natural range 24 down to 21;
-  subtype COL_ID2_RANGE is natural range 28 down to 25;
-  subtype ROW_ID_RANGE is natural range 32 down to 29;
-  subtype COL_ID_RANGE is natural range 36 down to 33;
-  subtype EA_RANGE is natural range 38 down to 37;
+  subtype TOT_RANGE is natural range 8 downto 0;
+  subtype CAL_RANGE is natural range 18 downto 9;
+  subtype TOA_RANGE is natural range 29 downto 19;
+  subtype COUNTER_A_RANGE is natural range 8 downto 0;
+  subtype BCID_RANGE is natural range 20 downto 9;
+  subtype ROW_ID2_RANGE is natural range 24 downto 21;
+  subtype COL_ID2_RANGE is natural range 28 downto 25;
+  subtype ROW_ID_RANGE is natural range 32 downto 29;
+  subtype COL_ID_RANGE is natural range 36 downto 33;
+  subtype EA_RANGE is natural range 38 downto 37;
 
   -- trailer
-  subtype CRC_RANGE is natural range 7 down to 0;
-  subtype HITS_RANGE is natural range 15 down to 8;
-  subtype STATUS_RANGE is natural range 21 down to 16;
-  subtype CHIPID_RANGE is natural range 38 down to 22;
+  subtype CRC_RANGE is natural range 7 downto 0;
+  subtype HITS_RANGE is natural range 15 downto 8;
+  subtype STATUS_RANGE is natural range 21 downto 16;
+  subtype CHIPID_RANGE is natural range 38 downto 22;
 
 end package dataformat_pkg;
--- src/etroc/configs/dataformat_parser.py
+++ src/etroc/configs/dataformat_parser.py
@@ -20,15 +20,25 @@ def mask2range(num):
     binnum = bin(num)[2:]
     start = len(binnum) - 1
     end = len(binnum) - binnum.rfind("1") - 1
-    return str(start)+" down to "+str(end)
+    return str(start)+" downto "+str(end)
 
 def defConstants(dat):
     data = dat["identifiers"]
+
+    max = 0
     for const in data:
         for typ in data[const]:
-            vhd.write("constant "+str.upper(const)+"_IDENTIFIER_"
-                +str.upper(typ)+"\t: std_logic_vector (39 downto 0) := "
-                +toVHDhex(data[const][typ], 10)+";\n")
+            length = len(str.upper(const) + str.upper(typ))
+            if (length > max):
+                max = length
+
+    for const in data:
+        for typ in data[const]:
+            vhd.write("  constant %s_IDENTIFIER_%s%s: std_logic_vector (39 downto 0) := %s;\n" % (
+                    str.upper(const),
+                    str.upper(typ),
+                    " "*(max-len(str.upper(const) + str.upper(typ))),
+                    toVHDhex(data[const][typ], 10)))
 
 def defSubTypes(dat, name):
     data = dat["data"][name]
@@ -47,15 +57,17 @@ vhd.write("use ieee.std_logic_misc.all;\n")
 vhd.write("use ieee.numeric_std.all;\n")
 vhd.write("\n")
 
-vhd.write("constant REVERSE : boolean := ")
+vhd.write("package "+filename+"_pkg is"+"\n")
+
+vhd.write("\n")
+
+vhd.write("  constant REVERSE : boolean := ")
 if ROC2dat["bitorder"] == "reversed": vhd.write("true")
 else: vhd.write("false")
 vhd.write(";\n\n")
 
 defConstants(ROC2dat)
-vhd.write("\n")
 
-vhd.write("package "+filename+"_pkg is"+"\n")
 vhd.write("\n")
 
 defSubTypes(ROC2dat, "header")
