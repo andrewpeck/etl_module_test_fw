93186ca295d3393de16ce76ee50ce035ab048a3d
Andrew Peck <andrew.peck@cern.ch>
2023-05-22 16:03:51 -0400

    update fast commands

--- src/etroc/configs/fast_commands.vhd
+++ src/etroc/configs/fast_commands.vhd
@@ -10,4 +10,8 @@ package fast_commands_pkg is
   constant L1A_CMD        : std_logic_vector(7 downto 0) := x"96";
   constant L1ABCR_CMD     : std_logic_vector(7 downto 0) := x"99";
   constant BCR_CMD        : std_logic_vector(7 downto 0) := x"5A";
+  constant STP_CMD        : std_logic_vector(7 downto 0) := x"55";
+  constant WS_START_CMD   : std_logic_vector(7 downto 0) := x"A5";
+  constant WS_STOP_CMD    : std_logic_vector(7 downto 0) := x"AA";
+  constant INJQ_CMD       : std_logic_vector(7 downto 0) := x"69";
 end package fast_commands_pkg;
