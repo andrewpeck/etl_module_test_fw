b79cd9300d0ff4b75049a712ef272211d289d11a
Andrew Peck <andrew.peck@cern.ch>
2023-07-08 08:47:06 -0400

    add back in the bitfile
    this is not supported by hog CI at the moment... :(

--- Top/etl_test_fw/post-bitstream.tcl
+++ Top/etl_test_fw/post-bitstream.tcl
@@ -5,7 +5,8 @@ set describe [GetHogDescribe $sha]
 file copy -force [file normalize $dst_dir/../../scripts/program.tcl] $dst_dir/program.tcl
 file copy -force [file normalize $dst_dir/../../scripts/program.sh] $dst_dir/program.sh
 
-file delete [glob "$dst_dir/*.bit"]
+#remove the bit file from the archive since it is redundant with the bin
+#file delete [glob "$dst_dir/*.bit"]
 
 # write_cfgmem -force -format mcs -size 64 -interface SPIx8 -loadbit \
 #     "up 0x00000000 $dst_bit" -file $dst_mcs
